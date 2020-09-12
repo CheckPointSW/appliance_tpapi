c05e4b2b2f67ffa9d161f99124fe4c81da2c0bcc
oferfic <54061076+oferfic@users.noreply.github.com>
2020-09-12 22:19:22 +0300

    appliance_tpapi

---
+++ av_api/av_file_handler.py
@@ -0,0 +1,157 @@
+import json
+import requests
+import os
+import hashlib
+import time
+import copy
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+SECONDS_TO_WAIT = 2
+MAX_RETRIES = 20
+
+
+class AV(object):
+    """
+    This class gets a file as input. The methods will query AV cache for already existing results of the file md5,
+     and in case not then upload the file to TED, query until an answer with AV results is received,
+     and parse the last response.
+    """
+    def __init__(self, url, file_name, file_path, output_folder):
+        self.url = url
+        self.file_name = file_name
+        self.file_path = file_path
+        self.request_template = {
+            "request": [{
+                "features": ["av"]
+            }]
+        }
+        self.output_folder = output_folder
+        self.md5 = ""
+        self.final_response = ""
+        self.final_status_label = ""
+        self.report_id = ""
+
+    def set_file_md5(self):
+        """
+        Calculates the file's md5
+        """
+        md5 = hashlib.md5()
+        with open(self.file_path, 'rb') as f:
+            while True:
+                block = f.read(2 ** 10)  # One-megabyte blocks
+                if not block:
+                    break
+                md5.update(block)
+            self.md5 = md5.hexdigest()
+
+    def create_response_info(self, response):
+        """
+        Create the AV response info of handled file.
+        :param response: last response
+        """
+        output_path = os.path.join(self.output_folder, self.file_name)
+        output_path += ".response.txt"
+        with open(output_path, 'w') as file:
+            file.write(json.dumps(response))
+
+    def check_av_cache(self):
+        """
+        Query (for av) the file (before upload) in order to find whether file results already exist in AV cache.
+        :return the query response
+        """
+        self.set_file_md5()
+        request = copy.deepcopy(self.request_template)
+        request['request'][0]['md5'] = self.md5
+        print("file md5: {}".format(self.md5))
+        data = json.dumps(request)
+        print("Sending AV Query request before upload in order to check AV cache")
+        response = requests.post(url=self.url + "query", data=data, verify=False)
+        response_j = response.json()
+        return response_j
+
+    def upload_file(self):
+        """
+        Upload the file to the appliance for av and get the upload response.
+        :return the upload response
+        """
+        request = copy.deepcopy(self.request_template)
+        data = json.dumps(request)
+        curr_file = {
+            'request': data,
+            'file': open(self.file_path, 'rb')
+        }
+        print("Sending Upload request for av")
+        try:
+            response = requests.post(url=self.url + "upload", files=curr_file, verify=False)
+        except Exception as E:
+            print("Upload file failed. file: {} , failure: {}. will continue".format(self.file_name, E))
+            raise
+        response_j = response.json()
+        print("av Upload response status: {}".format(response_j["response"][0]["status"]["label"]))
+        return response_j
+
+    def query_file(self):
+        """
+        Query the appliance for av of the file every SECONDS_TO_WAIT seconds.
+        Repeat query until receiving av results.
+        :return the (last) query response with the handled file AV results
+        """
+        print("Start sending Query requests for av after AV upload")
+        request = copy.deepcopy(self.request_template)
+        request['request'][0]['md5'] = self.md5
+        data = json.dumps(request)
+        response_j = json.loads('{}')
+        status_label = False
+        retry_no = 0
+        while (not status_label) or (status_label == "NOT_FOUND"):
+            print("Sending Query request for av")
+            response = requests.post(url=self.url + "query", data=data, verify=False)
+            response_j = response.json()
+            status_label = response_j['response'][0]['status']['label']
+            if status_label != "NOT_FOUND":
+                break
+            print("av Query response status still not found")
+            time.sleep(SECONDS_TO_WAIT)
+            retry_no += 1
+            if retry_no == MAX_RETRIES:
+                print("Reached query max retries.  Stop waiting for av results")
+                break
+        return response_j
+
+    def handle_file(self):
+        """
+        1. Query AV cache for already existing results of the file md5.
+            If results exist then goto #4, otherwise- continue to #2
+        2. Upload the file to the appliance for av feature.
+        3. If upload result is upload_success then query av until receiving results.
+        4. Write to file last query/upload response info.
+        5. If av result is found malicious then print the resulted av signature.
+        """
+        query_cache_response = self.check_av_cache()
+        cache_status_label = query_cache_response['response'][0]['status']['label']
+        if cache_status_label == "FOUND":
+            print("Results already exist in AV cache")
+            self.final_response = query_cache_response
+            self.final_status_label = cache_status_label
+        else:
+            print("No results in AV cache before upload")
+            upload_response = self.upload_file()
+            upload_status_label = upload_response["response"][0]["status"]["label"]
+            if upload_status_label == "UPLOAD_SUCCESS":
+                query_response = self.query_file()
+                query_status_label = query_response["response"][0]["status"]["label"]
+                print("Receiving Query response with av results. status: {}".format(query_status_label))
+                self.final_response = query_response
+                self.final_status_label = query_status_label
+            else:
+                self.final_response = upload_response
+                self.final_status_label = upload_status_label
+        self.create_response_info(self.final_response)
+        if self.final_status_label == "FOUND":
+            signature = self.final_response["response"][0]["av"]["malware_info"]["signature_name"]
+            if signature:
+                print("File {} was found malicious by AV.  Signature : {}".format(self.file_name, signature))
+            else:
+                print("File {} was found clean by AV".format(self.file_name))
