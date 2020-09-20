bd2891ad2dbc60156528030b858859b4dcdcaf5b
chkp-oferf <71375601+chkp-oferf@users.noreply.github.com>
2020-09-20 17:27:51 +0300

    appliance_tpapi

--- av_api/av_file_handler.py
+++ av_api/av_file_handler.py
@@ -14,9 +14,13 @@ MAX_RETRIES = 20
 
 class AV(object):
     """
-    This class gets a file as input. The methods will query AV cache for already existing results of the file md5,
-     and in case not then upload the file to TED, query until an answer with AV results is received,
-     and parse the last response.
+    This class gets a file as input and handles it as follows (function handle_file) :
+     1. Query AV cache by the file md5 for already existing AV results.
+     2. If not found in AV cache then :
+       2.1 Upload the file to the appliance for handling by av feature.
+       2.2 If upload result is upload_success (meaning no AV results yet) then :
+             Query av feature until receiving AV results.
+     3. Write the AV results (last query/upload response info) into the relevant output folder.
     """
     def __init__(self, url, file_name, file_path, output_folder):
         self.url = url
@@ -48,7 +52,7 @@ class AV(object):
 
     def create_response_info(self, response):
         """
-        Create the AV response info of handled file.
+        Create the AV response info of handled file and write it into the output folder.
         :param response: last response
         """
         output_path = os.path.join(self.output_folder, self.file_name)
@@ -64,9 +68,9 @@ class AV(object):
         self.set_file_md5()
         request = copy.deepcopy(self.request_template)
         request['request'][0]['md5'] = self.md5
-        print("file md5: {}".format(self.md5))
+        print("file {} md5: {}".format(self.file_name, self.md5))
         data = json.dumps(request)
-        print("Sending AV Query request before upload in order to check AV cache")
+        print("Sending AV Query request before upload in order to check AV cache for file {}".format(self.file_name))
         response = requests.post(url=self.url + "query", data=data, verify=False)
         response_j = response.json()
         return response_j
@@ -82,14 +86,15 @@ class AV(object):
             'request': data,
             'file': open(self.file_path, 'rb')
         }
-        print("Sending Upload request for av")
+        print("Sending Upload request of av for file {}".format(self.file_name))
         try:
             response = requests.post(url=self.url + "upload", files=curr_file, verify=False)
         except Exception as E:
-            print("Upload file failed. file: {} , failure: {}. will continue".format(self.file_name, E))
+            print("Upload file failed. file: {} , failure: {}".format(self.file_name, E))
             raise
         response_j = response.json()
-        print("av Upload response status: {}".format(response_j["response"][0]["status"]["label"]))
+        print("av Upload response status for file {} : {}".format(self.file_name,
+                                                                  response_j["response"][0]["status"]["label"]))
         return response_j
 
     def query_file(self):
@@ -98,7 +103,7 @@ class AV(object):
         Repeat query until receiving av results.
         :return the (last) query response with the handled file AV results
         """
-        print("Start sending Query requests for av after AV upload")
+        print("Start sending Query requests of av after AV upload for file {}".format(self.file_name))
         request = copy.deepcopy(self.request_template)
         request['request'][0]['md5'] = self.md5
         data = json.dumps(request)
@@ -106,43 +111,39 @@ class AV(object):
         status_label = False
         retry_no = 0
         while (not status_label) or (status_label == "NOT_FOUND"):
-            print("Sending Query request for av")
+            print("Sending Query request for av for file {}".format(self.file_name))
             response = requests.post(url=self.url + "query", data=data, verify=False)
             response_j = response.json()
             status_label = response_j['response'][0]['status']['label']
             if status_label != "NOT_FOUND":
                 break
-            print("av Query response status still not found")
+            print("av Query response status for file {} is still pending".format(self.file_name))
             time.sleep(SECONDS_TO_WAIT)
             retry_no += 1
             if retry_no == MAX_RETRIES:
-                print("Reached query max retries.  Stop waiting for av results")
+                print("Reached query max retries.  Stop waiting for av results for file {}".format(self.file_name))
                 break
         return response_j
 
     def handle_file(self):
         """
-        1. Query AV cache for already existing results of the file md5.
-            If results exist then goto #4, otherwise- continue to #2
-        2. Upload the file to the appliance for av feature.
-        3. If upload result is upload_success then query av until receiving results.
-        4. Write to file last query/upload response info.
-        5. If av result is found malicious then print the resulted av signature.
+        (Function description is within above class description)
         """
         query_cache_response = self.check_av_cache()
         cache_status_label = query_cache_response['response'][0]['status']['label']
         if cache_status_label == "FOUND":
-            print("Results already exist in AV cache")
+            print("Results already exist in AV cache for file {}".format(self.file_name))
             self.final_response = query_cache_response
             self.final_status_label = cache_status_label
         else:
-            print("No results in AV cache before upload")
+            print("No results in AV cache before upload for file {}".format(self.file_name))
             upload_response = self.upload_file()
             upload_status_label = upload_response["response"][0]["status"]["label"]
             if upload_status_label == "UPLOAD_SUCCESS":
                 query_response = self.query_file()
                 query_status_label = query_response["response"][0]["status"]["label"]
-                print("Receiving Query response with av results. status: {}".format(query_status_label))
+                print("Receiving Query response with av results for file {}. status: {}".format(self.file_name,
+                                                                                                query_status_label))
                 self.final_response = query_response
                 self.final_status_label = query_status_label
             else:
