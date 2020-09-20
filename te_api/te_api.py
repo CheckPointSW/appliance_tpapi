91ffc21f8f42d94085f71835bd69d749fd87fe0e
chkp-oferf <71375601+chkp-oferf@users.noreply.github.com>
2020-09-20 17:28:30 +0300

    appliance_tpapi

--- te_api/te_api.py
+++ te_api/te_api.py
@@ -1,8 +1,8 @@
 """
 te_api
     A Python client side utility for using Threat Emulation API calls to an appliance.
-    
-     You may either set the global variables below (some or all), or assigning their optional
+
+    You may either set the global variables below (some or all), or assigning their optional
       arguments when running the utility.  Run  te_api --help  for the arguments details.
 """
 
@@ -17,7 +17,12 @@ appliance_ip = "NNN.NNN.NNN.NNN"
 
 
 def main():
-    # Arguments handling
+    """
+    1. Get the optional arguments (if any): the input-directory, the output-root-directory and appliance-ip.
+    2. Accordingly set the api-url, and create the output directory.
+    3. Go though all input files in the input directory.
+        Handling each input file is described in TE class in te_file_handler.py:
+    """
     global input_directory
     global output_directory
     global appliance_ip
@@ -41,14 +46,14 @@ def main():
             os.mkdir(output_directory)
         except Exception as E1:
             print("could not create te_api output directory, because: {}".format(E1))
-            raise
+            return
     if args.appliance_ip:
         appliance_ip = args.appliance_ip
     print("The appliance ip address : {}".format(appliance_ip))
     url = "https://" + appliance_ip + ":18194/tecloud/api/v1/file/"
 
-    # A loop over the files in the input directory
-    print("Handle input files")
+    # A loop over the files in the input folder
+    print("Begin handling input files by TE")
     for file_name in os.listdir(input_directory):
         try:
             full_path = os.path.join(input_directory, file_name)
@@ -56,7 +61,7 @@ def main():
             te = TE(url, file_name, full_path, output_directory)
             te.handle_file()
         except Exception as E:
-            print("could not handle file: {} because: {}. will continue".format(file_name, E))
+            print("could not handle file: {} because: {}. Continue to handle next file.".format(file_name, E))
             continue
 
 
