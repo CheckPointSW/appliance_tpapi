4d94e5ab1cd94b6aa05f3242d72310c090a9b636
chkp-oferf <71375601+chkp-oferf@users.noreply.github.com>
2020-10-08 01:03:28 +0300

    appliance_tpapi

--- te_api/te_file_handler.py
+++ te_api/te_file_handler.py
@@ -14,17 +14,6 @@ SECONDS_TO_WAIT = 5
 MAX_RETRIES = 120
 
 
-def parse_verdict(response, feature):
-    """
-    Parsing the verdict of handled feature results response, in case the that feature response status is FOUND.
-    :param response: the handled response
-    :param feature: either "te" or "te_eb"
-    """
-    verdict = response["response"][0][feature]["combined_verdict"]
-    print("{} verdict is: {}".format(feature, verdict))
-    return verdict
-
-
 class TE(object):
     """
     This class gets a file as input and handles it as follows (function handle_file) :
@@ -33,8 +22,7 @@ class TE(object):
        2.1 Upload the file to the appliance for handling by te and te_eb features.
        2.2 If upload result is upload_success (meaning no TE results yet) then :
              Query te and te_eb features until receiving TE results.
-               If in between receiving te_eb found results of the early malicious verdict, then report online the
-                early malicious verdict result.
+               If in between receiving te_eb found results of the early malicious verdict, then display the verdict.
      3. Write the TE results (last query/upload response info) into the output folder.
           If resulted TE verdict is malicious then also download the TE report and write it into the output folder.
     """
@@ -59,6 +47,12 @@ class TE(object):
         self.final_status_label = ""
         self.report_id = ""
 
+    def print(self, msg):
+        """
+        Logging purpose
+        """
+        print("file {} : {}".format(self.file_name, msg))
+
     def set_file_sha1(self):
         """
         Calculates the file's sha1
@@ -72,6 +66,17 @@ class TE(object):
                 sha1.update(block)
             self.sha1 = sha1.hexdigest()
 
+    def parse_verdict(self, response, feature):
+        """
+        Parsing the verdict of handled feature results response, in case the that feature response status is FOUND.
+        :param response: the handled response
+        :param feature: either "te" or "te_eb"
+        :return the verdict
+        """
+        verdict = response["response"][0][feature]["combined_verdict"]
+        self.print("{} verdict is: {}".format(feature, verdict))
+        return verdict
+
     def parse_report_id(self, response):
         """
         parse and return the summary report id
@@ -80,7 +85,7 @@ class TE(object):
         try:
             self.report_id = response["response"][0]["te"]["summary_report"]
         except Exception as E:
-            print("Could not get TE report id, failure: {}. ".format(E))
+            self.print("Could not get TE report id, failure: {}. ".format(E))
 
     def create_response_info(self, response):
         """
@@ -101,9 +106,9 @@ class TE(object):
         request = copy.deepcopy(self.request_template)
         request['request'][0]['features'].remove('te_eb')
         request['request'][0]['sha1'] = self.sha1
-        print("file {} sha1: {}".format(self.file_name, self.sha1))
+        self.print("sha1: {}".format(self.sha1))
         data = json.dumps(request)
-        print("Sending TE Query request before upload in order to check TE cache for file {}".format(self.file_name))
+        self.print("Sending TE Query request before upload in order to check TE cache")
         response = requests.post(url=self.url + "query", data=data, verify=False)
         response_j = response.json()
         return response_j
@@ -119,15 +124,14 @@ class TE(object):
             'request': data,
             'file': open(self.file_path, 'rb')
         }
-        print("Sending Upload request of te and te_eb for file {}".format(self.file_name))
+        self.print("Sending Upload request of te and te_eb")
         try:
             response = requests.post(url=self.url + "upload", files=curr_file, verify=False)
         except Exception as E:
-            print("Upload file failed. file: {} , failure: {}".format(self.file_name, E))
+            self.print("Upload file failed: {}".format(E))
             raise
         response_j = response.json()
-        print("te and te_eb Upload response status for file {} : {}".format(self.file_name,
-                                                                            response_j["response"][0]["status"]["label"]))
+        self.print("te and te_eb Upload response status : {}".format(response_j["response"][0]["status"]["label"]))
         return response_j
 
     def query_file(self):
@@ -136,7 +140,7 @@ class TE(object):
         Repeat query until receiving te results.  te_eb results of early malicious verdict might be received earlier.
         :return the (last) query response with the handled file TE results
         """
-        print("Start sending Query requests of te and te_eb after TE upload for file {}".format(self.file_name))
+        self.print("Start sending Query requests of te and te_eb after TE upload")
         request = copy.deepcopy(self.request_template)
         request['request'][0]['sha1'] = self.sha1
         data = json.dumps(request)
@@ -145,7 +149,7 @@ class TE(object):
         te_eb_found = False
         retry_no = 0
         while (not status_label) or (status_label == "PENDING") or (status_label == "PARTIALLY_FOUND"):
-            print("Sending Query request of te and te_eb for file {}".format(self.file_name))
+            self.print("Sending Query request of te and te_eb")
             response = requests.post(url=self.url + "query", data=data, verify=False)
             response_j = response.json()
             status_label = response_j['response'][0]['status']['label']
@@ -156,10 +160,10 @@ class TE(object):
                     te_eb_status_label = response_j["response"][0]["te_eb"]['status']['label']
                     if te_eb_status_label == "FOUND":
                         te_eb_found = True
-                        te_eb_verdict = parse_verdict(response_j, "te_eb")
+                        te_eb_verdict = self.parse_verdict(response_j, "te_eb")
                         if te_eb_verdict == "Malicious":
-                            print("Early verdict is malicious for file {}".format(self.file_name))
-                            print("Continue Query until receiving te results for file {}".format(self.file_name))
+                            self.print("Early verdict is malicious")
+                            self.print("Continue Query until receiving te results")
                 te_status_label = response_j["response"][0]["te"]['status']['label']
                 if (te_status_label == "FOUND") or (te_status_label == "NOT_FOUND"):
                     break
@@ -172,11 +176,11 @@ class TE(object):
                             break
                     if no_pending_image:
                         break
-            print("te and te_eb Query response status for file {} : {}".format(self.file_name, status_label))
+            self.print("te and te_eb Query response status : {}".format(status_label))
             time.sleep(SECONDS_TO_WAIT)
             retry_no += 1
             if retry_no == MAX_RETRIES:
-                print("Reached query max retries.  Stop waiting for te results for file {}".format(self.file_name))
+                self.print("Reached query max retries.  Stop waiting for te results for")
                 break
         return response_j
 
@@ -186,7 +190,7 @@ class TE(object):
          into the output folder
         """
         try:
-            print("Sending Download request for TE report for file {}".format(self.file_name))
+            self.print("Sending Download request for TE report")
             response = requests.get(url=self.url + "download?id=" + self.report_id, verify=False)
             encoded_content_string = response.text
             decoded_content = base64.b64decode(encoded_content_string)
@@ -199,9 +203,9 @@ class TE(object):
             report_tar = tarfile.open(decoded_report_archive_path)
             report_tar.extractall(report_dir)
             report_tar.close()
-            print("TE report for file {} is in sub-directory: {}".format(self.file_name, report_dir))
+            self.print("TE report is in sub-directory: {}".format(report_dir))
         except Exception as E:
-            print("Downloading TE report for file {} failed:  {} ".format(self.file_name, E))
+            self.print("Downloading TE report failed:  {} ".format(E))
 
     def handle_file(self):
         """
@@ -210,18 +214,17 @@ class TE(object):
         query_cache_response = self.check_te_cache()
         cache_status_label = query_cache_response['response'][0]['status']['label']
         if cache_status_label == "FOUND":
-            print("Results already exist in TE cache for file {}".format(self.file_name))
+            self.print("Results already exist in TE cache")
             self.final_response = query_cache_response
             self.final_status_label = cache_status_label
         else:
-            print("No results in TE cache before upload for file {}".format(self.file_name))
+            self.print("No results in TE cache before upload")
             upload_response = self.upload_file()
             upload_status_label = upload_response["response"][0]["status"]["label"]
             if upload_status_label == "UPLOAD_SUCCESS":
                 query_response = self.query_file()
                 query_status_label = query_response["response"][0]["status"]["label"]
-                print("Receiving Query response with te results for file {}. status: {}".format(self.file_name,
-                                                                                                query_status_label))
+                self.print("Receiving Query response with te results. status: {}".format(query_status_label))
                 self.final_response = query_response
                 self.final_status_label = query_status_label
             else:
@@ -229,7 +232,7 @@ class TE(object):
                 self.final_status_label = upload_status_label
         self.create_response_info(self.final_response)
         if self.final_status_label == "FOUND":
-            verdict = parse_verdict(self.final_response, "te")
+            verdict = self.parse_verdict(self.final_response, "te")
             if verdict == "Malicious":
                 self.parse_report_id(self.final_response)
                 if self.report_id != "":
